9f8816cb335e5310c754390309ec2a01b16b1ebc
Kriszta <matyi.kriszta@gmail.com>
2014-06-25 20:05:08 +0100

    created app, added rails_admin and devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PolishApp::Application.config.secret_token = '92f6ac04d9e5c3ce0ba0ce37f72b1b4f0255c441cfa8ad3b526c82e91d9464b03892bfab09f1e1ec817b71705d998eedf0fe849f7df2a0e04b8e67dab8d56428'
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+PolishApp::Application.routes.draw do
+  devise_for :users
+
+  mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
+
+  root to: "home#index"
+  
+end
